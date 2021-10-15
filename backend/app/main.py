3e87f08ef85303a3ec4d8012d03d54ad399be35e
Martin Tang <62964506+TangMartin@users.noreply.github.com>
2021-10-15 06:33:44 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "An example GET"}
 
-@app.get("/getname")
-async def root():
-    return {"name": "Martin T."}
-
 @app.post("/")
 async def root():
     return {"message": "An example POST"}

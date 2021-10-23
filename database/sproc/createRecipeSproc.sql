0df9e49afe75950005031c54a4e63e64316ea9d7
Nick Kao <kobenkao@gmail.com>
2021-10-23 14:01:05 -0700

    stylized create recipe sproc

--- database/sproc/createRecipeSproc.sql
+++ database/sproc/createRecipeSproc.sql
@@ -4,10 +4,47 @@ DROP procedure IF EXISTS `createRecipe`;
 
 DELIMITER $$
 USE `umami_db`$$
-CREATE PROCEDURE `createRecipe` (IN `_recipe_id` INT, IN `_name` VARCHAR(50), IN `_created_time` DATETIME, IN `_user_id` VARCHAR(50), IN `_protein` INT, IN `_carbs` INT, IN `_calories` INT, IN `_servings` INT, IN `_vegetarian` BOOLEAN, IN `_vegan` BOOLEAN, IN `_cooking_time` INT)
+CREATE PROCEDURE `createRecipe` (
+    IN `_recipe_id` INT,
+    IN `_name` VARCHAR(50),
+    IN `_created_time` DATETIME,
+    IN `_user_id` VARCHAR(50),
+    IN `_protein` INT,
+    IN `_carbs` INT,
+    IN `_calories` INT,
+    IN `_servings` INT,
+    IN `_vegetarian` BOOLEAN,
+    IN `_vegan` BOOLEAN,
+    IN `_cooking_time` INT
+)
 BEGIN
 
-REPLACE INTO `recipes_table` (`recipe_id`, `name`, `created_time`, `user_id`, `protein`, `carbs`, `calories`, `servings`, `vegetarian`, `vegan`, `cooking_time`)
-VALUES (`_recipe_id`, `_name`, `_created_time`, `_user_id`, `_protein`, `_carbs`, `_calories`, `_servings`, `_vegetarian`, `_vegan`, `_cooking_time`)
+REPLACE INTO `recipes_table` (
+    `recipe_id`,
+    `name`,
+    `created_time`,
+    `user_id`,
+    `protein`,
+    `carbs`,
+    `calories`,
+    `servings`,
+    `vegetarian`,
+    `vegan`,
+    `cooking_time`
+)
+VALUES (
+    `_recipe_id`,
+    `_name`,
+    `_created_time`,
+    `_user_id`,
+    `_protein`,
+    `_carbs`,
+    `_calories`,
+    `_servings`,
+    `_vegetarian`,
+    `_vegan`,
+    `_cooking_time`)
 
 END$$
+
+DELIMITER ;

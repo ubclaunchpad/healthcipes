59f313be17e0eb73f15accc6d38b8aa0647817ca
Nick Kao <kobenkao@gmail.com>
2021-10-23 13:54:55 -0700

    create recipe sproc renmae

--- database/sproc/createRecipeSproc.sql
+++ database/sproc/createRecipeSproc.sql
@@ -1,10 +1,10 @@
 USE `umami_db`;
 
-DROP procedure IF EXISTS `postRecipe`;
+DROP procedure IF EXISTS `createRecipe`;
 
 DELIMITER $$
 USE `umami_db`$$
-CREATE PROCEDURE `postRecipe` (IN `_recipe_id` INT, IN `_name` VARCHAR(50), IN `_created_time` DATETIME, IN `_user_id` VARCHAR(50), IN `_protein` INT, IN `_carbs` INT, IN `_calories` INT, IN `_servings` INT, IN `_vegetarian` BOOLEAN, IN `_vegan` BOOLEAN, IN `_cooking_time` INT)
+CREATE PROCEDURE `createRecipe` (IN `_recipe_id` INT, IN `_name` VARCHAR(50), IN `_created_time` DATETIME, IN `_user_id` VARCHAR(50), IN `_protein` INT, IN `_carbs` INT, IN `_calories` INT, IN `_servings` INT, IN `_vegetarian` BOOLEAN, IN `_vegan` BOOLEAN, IN `_cooking_time` INT)
 BEGIN
 
 REPLACE INTO `recipes_table` (`recipe_id`, `name`, `created_time`, `user_id`, `protein`, `carbs`, `calories`, `servings`, `vegetarian`, `vegan`, `cooking_time`)

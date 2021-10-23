e8dd59b75ee437e70fd0b3bef0a2ec267b6ee614
Nick Kao <kobenkao@gmail.com>
2021-10-23 14:14:42 -0700

    creat indexer for create recipe sproc

--- backend/app/indexer/recipes.py
+++ backend/app/indexer/recipes.py
@@ -15,9 +15,45 @@ def get_recipe_by_keyword(cursor, keyword):
 def get_all_recipes(cursor):
     sql_proc = 'getAllRecipes'
     try:
-        cursor.execute("SHOW PROCEDURE STATUS WHERE Db = 'umami_db';")
-        # cursor.callproc(sql_proc)
+        # cursor.execute("SHOW PROCEDURE STATUS WHERE Db = 'umami_db';")
+        cursor.callproc(sql_proc)
         return cursor.fetchall()
+    except Exception as e:
+        print("MYSQL ERROR:", sql_proc)
+        logging.error(e)
+
+
+def post_recipe(conn, cursor, recipe):
+    sql_proc = 'createRecipe'
+
+    recipe_id = recipe['recipe_id']
+    name = recipe['name']
+    created_time = recipe['created_time']
+    user_id = recipe['user_id']
+    protein = recipe['protein']
+    carbs = recipe['carbs']
+    calories = recipe['calories']
+    servings = recipe['servings']
+    vegetarian = recipe['vegetarian']
+    vegan = recipe['vegan']
+    cooking_time = recipe['cooking_time']
+
+    try:
+        cursor.callproc(sql_proc, (
+            recipe_id,
+            name,
+            created_time,
+            user_id,
+            protein,
+            carbs,
+            calories,
+            servings,
+            vegetarian,
+            vegan,
+            cooking_time,
+        ))
+        conn.commit()
+        return recipe
     except Exception as e:
         print("MYSQL ERROR:", sql_proc)
         logging.error(e)

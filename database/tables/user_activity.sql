93235ffb6948de3ee0e8e844411295c10b3b56c5
Nick Kao <kobenkao@gmail.com>
2021-11-01 20:10:04 -0700

    sql commands and modification to deployment instructions

---
+++ database/tables/user_activity.sql
@@ -0,0 +1,25 @@
+USE `umami_db`;
+DROP procedure IF EXISTS `createUserActivity`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS `user_activity_table`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+DELIMITER $$
+
+USE `umami_db`$$
+CREATE PROCEDURE `user_activity_table` ()
+BEGIN
+
+CREATE TABLE `user_activity_table` (
+  `user_activity_id` INT NOT NULL auto_increment,
+  `user_id` VARCHAR(50) NOT NULL,
+  `date` DATETIME,
+  PRIMARY KEY (`user_activity_id`),
+  CONSTRAINT fk_user_image FOREIGN KEY (`user_id`)
+  REFERENCES `users_table`(`user_id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+END$$
+
+DELIMITER ;

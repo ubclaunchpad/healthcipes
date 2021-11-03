b309b34374dd30cf27cb9a6f2755c074a4d3b0d7
Nick Kao <kobenkao@gmail.com>
2021-11-02 17:59:29 -0700

    updated to include fks for different activities and enum for activity type

--- database/tables/user_activity.sql
+++ database/tables/user_activity.sql
@@ -14,10 +14,15 @@ BEGIN
 CREATE TABLE `user_activity_table` (
   `user_activity_id` INT NOT NULL auto_increment,
   `user_id` VARCHAR(50) NOT NULL,
+  `activity_type` ENUM(`RECIPE_LIKE`, `USER_FOLLOW`),
+  `user_follow_id` VARCHAR(50),
+  `recipe_like_id` INT,
   `date` DATETIME,
   PRIMARY KEY (`user_activity_id`),
-  CONSTRAINT fk_user_image FOREIGN KEY (`user_id`)
+  CONSTRAINT fk_user_follow_id FOREIGN KEY (`user_id`)
   REFERENCES `users_table`(`user_id`),
+  CONSTRAINT recipe_like_id FOREIGN KEY (`recipe_id`)
+  REFERENCES `recipes_table`(`recipe_id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 END$$

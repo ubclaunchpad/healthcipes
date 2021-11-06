9f7cfc5249b6e502f1f2483b61a7d3b8df6d38cd
HarinHaoWu <master.harin@gmail.com>
2021-11-06 14:25:59 -0700

    mock data

--- database/mock.sql
+++ database/mock.sql
@@ -2,13 +2,13 @@
 CALL umami_db.postUser('Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'harinwu', 'Harin', 'Wu', 'harinwu99@gmail.com', 'Vancouver', '', 1, 0, 0, 0, 0, 0, 0, 0, 'ANY', 0.5);
 CALL umami_db.postUser('G7YXRqIBU1QKLCXQnox8Wxwl2VN2', 'kayett', 'Kaye', 'T', 'kaye.thinh.to@gmail.com', 'Vancouver', '', 1, 0, 0, 0, 0, 0, 0, 0, 'ANY', 0.5);
 
-CALL umami_db.createRecipe(1, 'Butter Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Butter Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(2, 'Teriyaki Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Teriyaki Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(3, 'Fried Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Fried Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(4, 'Roasted Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Roasted Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(5, 'Spicy Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Spicy Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(6, 'Sweet & Sour Chicken', 'abc', 'gs://umami-2021.appspot.com/Recipes/Sweet & Sour Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(7, 'Beef Curry', 'abc', 'gs://umami-2021.appspot.com/Recipes/Beef Curry.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(8, 'Pineapple Gelato', 'abc', 'gs://umami-2021.appspot.com/Recipes/Pineapple Gelato.jpeg', 20, 10, 5, 2, 600, 3, 1, 1, 20);
-CALL umami_db.createRecipe(9, 'Kebabs', 'abc', 'gs://umami-2021.appspot.com/Recipes/Kebabs.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
-CALL umami_db.createRecipe(10, 'Fried Rice', 'abc', 'gs://umami-2021.appspot.com/Recipes/Fried Rice.jpeg', 20, 10, 5, 2, 600, 3, 1, 1, 20);
+CALL umami_db.createRecipe(1, 'Butter Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Butter Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(2, 'Teriyaki Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Teriyaki Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(3, 'Fried Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Fried Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(4, 'Roasted Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Roasted Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(5, 'Spicy Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Spicy Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(6, 'Sweet & Sour Chicken', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Sweet & Sour Chicken.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(7, 'Beef Curry', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Beef Curry.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(8, 'Pineapple Gelato', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Pineapple Gelato.jpeg', 20, 10, 5, 2, 600, 3, 1, 1, 20);
+CALL umami_db.createRecipe(9, 'Kebabs', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Kebabs.jpeg', 20, 10, 5, 2, 600, 3, 0, 0, 20);
+CALL umami_db.createRecipe(10, 'Fried Rice', 'Qnj6AjQOLoZlJw4TZBpRE3iNz0K3', 'gs://umami-2021.appspot.com/Recipes/Fried Rice.jpeg', 20, 10, 5, 2, 600, 3, 1, 1, 20);

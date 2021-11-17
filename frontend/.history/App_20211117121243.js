78f724ba367e3b9a224e1d4e332a43e51ed7ff09
Martin Tang <martintang8422@gmail.com>
2021-11-17 12:32:21 -0800

    Profile Tab Update

---
+++ frontend/.history/App_20211117121243.js
@@ -0,0 +1,200 @@
+import 'react-native-gesture-handler';
+import React, {useState, useEffect, useRef, useMemo} from 'react';
+import {enableScreens} from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import auth from '@react-native-firebase/auth';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignUp from './src/screens/login/signup';
+import Login from './src/screens/login/login';
+import Forgot from './src/screens/login/forgot';
+import ShoppingStyle from './src/screens/onboarding/shopping-style';
+import AboutYou from './src/screens/onboarding/about-you';
+import Feed from './src/screens/tab1/feed';
+import Pantry from './src/screens/tab2/pantry';
+import Post from './src/screens/tab3/post';
+import Notification from './src/screens/tab4/notification';
+import Profile from './src/screens/tab5/profile';
+import EditProfile from './src/screens/tab5/editprofile';
+import Diet from './src/screens/onboarding/diet';
+import TabsUI from './TabsUI';
+import Search from './src/screens/tab1/search';
+import Recipe from './src/screens/tab1/recipe';
+import EditPantry from './src/screens/tab2/editPantry';
+
+enableScreens();
+
+const FeedStack = createNativeStackNavigator();
+function FeedScreen() {
+  return (
+    <FeedStack.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <FeedStack.Screen name="Feed" component={Feed} />
+      <FeedStack.Screen name="Search" component={Search} />
+      <FeedStack.Screen name="Recipe" component={Recipe} />
+    </FeedStack.Navigator>
+  );
+}
+
+const NotificationStack = createNativeStackNavigator();
+function NotificationScreen() {
+  return (
+    <NotificationStack.Navigator
+      initialRouteName="Notification"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <NotificationStack.Screen name="Notification" component={Notification} />
+    </NotificationStack.Navigator>
+  );
+}
+
+const CreateStack = createNativeStackNavigator();
+function CreateScreen() {
+  return (
+    <CreateStack.Navigator
+      initialRouteName="Create"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <CreateStack.Screen name="Create" component={Post} />
+    </CreateStack.Navigator>
+  );
+}
+
+const PantryStack = createNativeStackNavigator();
+function PantryScreen() {
+  return (
+    <PantryStack.Navigator
+      initialRouteName="Pantry"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <PantryStack.Screen name="Pantry" component={Pantry} />
+      <PantryStack.Screen name="EditPantry" component={EditPantry} />
+    </PantryStack.Navigator>
+  );
+}
+
+const ProfileStack = createNativeStackNavigator();
+function ProfileScreen() {
+  return (
+    <ProfileStack.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <ProfileStack.Screen name="Profile" component={Profile} />
+      <ProfileStack.Screen name="EditProfile" component={EditProfile} />
+    </ProfileStack.Navigator>
+  );
+}
+
+const SignUpLoginStack = createNativeStackNavigator();
+const MasterStack = createNativeStackNavigator();
+
+export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const navigation = useRef();
+
+  const tabs = useMemo(
+    () => [
+      {
+        name: 'FeedTab',
+        action: () =>
+          navigation.current?.navigate('FeedTab', {route: 'FeedTab'}),
+      },
+      {
+        name: 'PantryTab',
+        action: () =>
+          navigation.current?.navigate('PantryTab', {route: 'PantryTab'}),
+      },
+      {
+        name: 'CreateTab',
+        action: () =>
+          navigation.current?.navigate('CreateTab', {route: 'CreateTab'}),
+      },
+      {
+        name: 'NotificationTab',
+        action: () =>
+          navigation.current?.navigate('NotificationTab', {
+            route: 'NotificationTab',
+          }),
+      },
+      {
+        name: 'ProfileTab',
+        action: () =>
+          navigation.current?.navigate('ProfileTab', {route: 'ProfileTab'}),
+      },
+    ],
+    [],
+  );
+
+  const Tab = createBottomTabNavigator();
+  function TabScreen() {
+    return (
+      <Tab.Navigator
+        tabBar={props => <TabsUI {...{tabs, ...props}} />}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Tab.Screen name="FeedTab" component={FeedScreen} />
+        <Tab.Screen name="PantryTab" component={PantryScreen} />
+        <Tab.Screen name="CreateTab" component={CreateScreen} />
+        <Tab.Screen name="NotificationTab" component={NotificationScreen} />
+        <Tab.Screen name="ProfileTab" component={ProfileScreen} />
+      </Tab.Navigator>
+    );
+  }
+
+  useEffect(() => {
+    // Handle user state changes
+    function onAuthStateChanged(newUser) {
+      setUser(newUser);
+      if (initializing) {
+        setInitializing(false);
+      }
+    }
+
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, [initializing]);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <SignUpLoginStack.Navigator
+          initialRouteName="SignUp"
+          screenOptions={{headerShown: false}}>
+          <SignUpLoginStack.Screen name="SignUp" component={SignUp} />
+          <SignUpLoginStack.Screen name="Login" component={Login} />
+          <SignUpLoginStack.Screen name="Forgot" component={Forgot} />
+        </SignUpLoginStack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  return (
+    <NavigationContainer ref={navigation}>
+      <MasterStack.Navigator
+        initialRouteName="MainTabs"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <MasterStack.Screen name="MainTabs" component={TabScreen} />
+        <MasterStack.Screen name="ShoppingStyle" component={ShoppingStyle} />
+        <MasterStack.Screen name="Diet" component={Diet} />
+        <MasterStack.Screen name="AboutYou" component={AboutYou} />
+      </MasterStack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ frontend/src/screens/tab5/editprofile.js
@@ -0,0 +1,101 @@
+import React, {useEffect, useRef, useMemo, useState} from 'react';
+import {
+  Text,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+  ImageBackground,
+  ColorPropType,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {useDispatch, useSelector} from 'react-redux';
+import GoButton from '../../components/goButton';
+import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
+import profileStyle from './profileStyle';
+import color from '../../styles/color';
+import {GET_USER} from '../../actions/accountActions';
+import { FlatList } from 'react-native-gesture-handler';
+import { csvParseRows } from 'd3-dsv';
+import { randomWeibull } from 'd3-random';
+
+export default function EditProfile({navigation}) {
+  const dispatch = useDispatch();
+  const onboarded = useSelector(state => state.globalReducer.onboardReducer);
+  const user = useSelector(state => state.accountReducer.userInfoReducer);
+  const featuredFeed = useSelector(
+    state => state.recipeReducer.featureFeedReducer,
+  );
+  const forYouFeed = useSelector(
+    state => state.recipeReducer.forYouFeedReducer,
+  );
+  const [page, setPage] = useState('Liked');
+  const bottomSheetRef = useRef(null);
+  const flatListRef = useRef(null);
+  const snapPoints = useMemo(() => ['70%'], []);
+
+  useEffect(() => {
+    dispatch({type: GET_USER, userID: auth().currentUser.uid});
+  }, [dispatch]);
+
+if (!onboarded) {
+    navigation.replace('ShoppingStyle');
+  } else {
+    return (
+      <SafeAreaView style={{flex: 1, backgroundColor: color.white,}}>
+        <FlatList
+          ListHeaderComponent={
+            <View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                }}>
+                <TouchableOpacity
+                  style={{
+                    flex: 1,
+                    marginTop: '10%',
+                    marginLeft: '5%',
+                  }}
+                  onPress={() => {
+                    navigation.pop();
+                  }}>
+                  <Image 
+                    source={require('../../assets/Back.png')}
+                    style={{
+                      resizeMode: 'contain',
+                      width: 30,
+                      height: 30,
+                      tintColor: color.black,
+                    }}
+                  />
+                </TouchableOpacity>
+                <Text
+                    style={{
+                      marginTop: '9.5%',
+                      fontSize: 30,
+                      color: 'black',
+                      fontWeight: 'bold',
+                      flex: 2.3,
+                      alignSelf: 'center',
+                    }}>
+                    Edit Profile
+                  </Text>
+              </View>
+              <View>
+                <Image 
+                  source={require('../../assets/Profilepicture.png')}
+                  style={profileStyle.editprofilePicture}
+                />
+              </View>
+          </View>
+          }
+      />
+      </SafeAreaView>
+    );
+  }
+
+  return null;
+}

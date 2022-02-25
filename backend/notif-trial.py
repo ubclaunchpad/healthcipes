65a443e26d929de8c30d608cc8508814166cbf2d
Nick Kao <kobenkao@gmail.com>
2022-02-25 15:29:20 -0800

    format

--- backend/notif-trial.py
+++ backend/notif-trial.py
@@ -11,17 +11,17 @@ serverToken = 'AAAAg7rOhRA:APA91bE2Wk_Ly0trmeTXKSgL3Epo-bo2G8A4TyaWETuTnFBZGZGNs
 deviceToken = 'd6fao-1Rmk5DorCUCtcy_O:APA91bG1jc985c2jmBgCSZXM3dz0v6u71UQGhlJDpexIet2cy8ifjsNLW85nBlPW6m_s7Xqb2KxFWi7IDeDthZljGJfqFAMP1G0T6axzDMnIkY0oNMXeeFyK1XzYl88oeCRRbt7jhITE'
 
 headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'key=' + serverToken,
+        "Content-Type": "application/json",
+        "Authorization": "key=" + serverToken,
       }
 
 body = {
-          'notification': {'title': 'Sending push form python script',
-                            'body': 'New Message'
+          "notification": {"title": "Sending push form python script",
+                            "body": "New Message"
                             },
-          'to':
+          "to":
               deviceToken,
-          'priority': 'high',
+          "priority": "high",
         #   'data': dataPayLoad,
         }
 response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))

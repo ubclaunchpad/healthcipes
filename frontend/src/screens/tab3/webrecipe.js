849bfff0bf285625c0f132e023fd7f5fcad0873b
Aaron <chan.aaron73@gmail.com>
2022-03-10 21:40:41 -0800

    Added components onto page.

--- frontend/src/screens/tab3/webrecipe.js
+++ frontend/src/screens/tab3/webrecipe.js
@@ -18,6 +18,7 @@ import { FlatList } from 'react-native-gesture-handler';
 import { POST_RECIPE } from '../../actions/recipeActions';
 import { API_URL } from '@env';
 import axios from 'axios';
+import color from '../../styles/color';
 
 export default function webrecipe({ navigation }) {
   const dispatch = useDispatch();
@@ -26,10 +27,16 @@ export default function webrecipe({ navigation }) {
   const [recipe, setRecipe] = useState({});
   const [steps, setSteps] = useState([]);
   const [ingredients, setIngredients] = useState([]);
+  const [recipeName, setRecipeName] = useState("");
+  const [image, setImage] = useState(recipe.header_image);
 
   useEffect(() => {
     dispatch({ type: GET_USER, userID: auth().currentUser.uid });
   }, [dispatch]);
+
+  useEffect(() => {
+    console.log(recipeName);
+  }, [recipeName]);
 
   function getRecipeFromURL(URL) {
     const apiConfig = {
@@ -50,9 +57,14 @@ export default function webrecipe({ navigation }) {
         setRecipe(result.data.recipe);
         setSteps(result.data.steps);
         setIngredients(result.data.ingredients);
+        setRecipeName(result.data.recipe.name);
+        setImage(result.data.recipe.header_image);
         console.log(recipe);
         console.log(ingredients);
         console.log(steps);
+        console.log(recipe.header_image);
+        console.log(recipeName);
+        console.log(typeof (image));
       })
       .catch(function (error) {
         console.log(error);
@@ -95,7 +107,7 @@ export default function webrecipe({ navigation }) {
               <View style={{
                 flexDirection: 'row'
               }}>
-                <View style={webrecipeStyle.textBox}>
+                {/* <View style={webrecipeStyle.textBox}>
                   <TouchableOpacity onPress={() => {
                     console.log(URL);
                     getRecipeFromURL(URL);
@@ -115,8 +127,48 @@ export default function webrecipe({ navigation }) {
                     onChangeText={(URL) => {
                       setURL(URL);
                     }} />
+                </View> */}
+                <View
+                  style={{
+                    marginTop: 30,
+                    backgroundColor: color.lightGray,
+                    height: 40,
+                    width: '85%',
+                    borderRadius: 20,
+                    paddingHorizontal: '5%',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    marginLeft: 30,
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      console.log(URL);
+                      getRecipeFromURL(URL);
+                    }}>
+                    <Image
+                      source={require('../../assets/Search.png')}
+                      style={{
+                        height: 20,
+                        width: 20,
+                        resizeMode: 'contain',
+                      }}
+                    />
+                  </TouchableOpacity>
+                  <TextInput
+                    style={{
+                      height: 40,
+                      paddingHorizontal: '5%',
+                      width: '100%',
+                    }}
+                    placeholder={"Search Web"}
+                    value={URL}
+                    onChangeText={(URL) => {
+                      console.log(URL);
+                      setURL(URL);
+                    }} />
+
                 </View>
-                <TouchableOpacity onPress={() => console.log("FILTER")}>
+                {/* <TouchableOpacity onPress={() => console.log("FILTER")}>
                   <Image
                     source={require("../../assets/Filter.png")}
                     style={{
@@ -126,10 +178,46 @@ export default function webrecipe({ navigation }) {
                       resizeMode: 'contain',
                       marginTop: 30,
                     }} />
-                </TouchableOpacity>
+                </TouchableOpacity> */}
               </View>
+              {/* <TouchableOpacity>
+                <Image
+                  style={{
+                    height: '50%',
+                    width: '50%',
+                    alignSelf: 'center',
+                    resizeMode: 'center',
+                  }}
+                  source={{ uri: image }} />
+              </TouchableOpacity> */}
               <View>
-                <Text>{recipe.name}</Text>
+                <View style={webrecipeStyle.textBox}>
+                  <TextInput
+                    style={webrecipeStyle.textInput}
+                    placeholder="Title"
+                    value={recipeName}
+                    onChangeText={(title) => {
+                      setRecipeName(title);
+                    }} />
+                </View>
+                <View style={webrecipeStyle.textBoxTall}>
+                  <TextInput
+                    style={webrecipeStyle.textInput}
+                    placeholder="Steps"
+                    value={steps}
+                    onChangeText={(steps) => {
+                      setSteps(steps);
+                    }} />
+                </View>
+                <View style={webrecipeStyle.textBoxTall}>
+                  <TextInput
+                    style={webrecipeStyle.textInput}
+                    placeholder="Ingredients"
+                    value={ingredients}
+                    onChangeText={(ingredients) => {
+                      setIngredients(ingredients);
+                    }} />
+                </View>
               </View>
               <View style={{
                 paddingHorizontal: 60,
@@ -141,11 +229,11 @@ export default function webrecipe({ navigation }) {
                   console.log(recipe);
                   console.log(steps);
                   console.log(ingredients);
-                  dispatch({ type: POST_RECIPE, recipeObj: recipe, steps: steps, ingredients: ingredients});
+                  dispatch({ type: POST_RECIPE, recipeObj: recipe, steps: steps, ingredients: ingredients });
                 })}
               </View>
             </View>} />
-      </SafeAreaView>
+      </SafeAreaView >
     );
   }
   return null;

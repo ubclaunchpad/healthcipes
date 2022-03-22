a93712f4396b4dd08647256288a426848636adfb
Ivan Fediaev <47201457+ivan-fediaev@users.noreply.github.com>
2022-03-21 17:34:00 -0700

    Recommender (#60)
    * Added recommender engine
    
    * Added recommender
    
    * Removed old recommender
    
    * removing files
    
    * Removed ds_store files
    
    * Removed last pycache files
    
    Co-authored-by: fediaev <fediaev@students.cs.ubc.ca>

---
+++ Recipe-Recommender/src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from sklearn.feature_extraction.text import TfidfVectorizer
+import rec_sys
+
+app = Flask(__name__)
+@app.route('/recipe', methods=["GET"])
+def recommend_recipe():
+    ingredients = request.args.get('ingredients')  
+    print(type(ingredients)) 
+    recipe = rec_sys.RecSys(ingredients)
+    
+    response = {}
+    count = 0
+    for index, row in recipe.iterrows():
+        response[count] = {
+            'recipe': str(row['recipe']),
+            'score': str(row['score']),
+            'ingredients': str(row['ingredients']),
+            'url': str(row['url'])
+        }
+        count += 1
+    return jsonify(response)
+   
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
+
+# http://127.0.0.1:5000/recipe?ingredients=pasta
